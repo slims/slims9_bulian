af10eee22a3d28dfccb76af901b732104f48fe43
Eddy Subratha <eddy.subratha@gmail.com>
2015-03-16 06:35:32 +0700

    Edit default OPAC theme

--- template/default/index_template.inc.php
+++ template/default/index_template.inc.php
@@ -66,7 +66,7 @@ if (!defined('INDEX_AUTH')) {
   <link href="<?php echo $sysconf['template']['dir']; ?>/core.style.css" rel="stylesheet" type="text/css" />
   <link href="<?php echo JWB; ?>colorbox/colorbox.css" rel="stylesheet" type="text/css" />
   <link href="<?php echo $sysconf['template']['css']; ?>" rel="stylesheet" type="text/css" />
-  <link type="text/css" rel="stylesheet" media="all" href="<?php echo SWB; ?>template/default/css/tango/skin.css"/>
+  <!-- <link type="text/css" rel="stylesheet" media="all" href="<?php echo SWB; ?>template/default/css/tango/skin.css"/> -->
   <?php echo $metadata; ?>
 
   <!-- Script
@@ -80,42 +80,11 @@ if (!defined('INDEX_AUTH')) {
 
 <!-- Header
 ============================================= -->
-
-<header class="s-header container" role="header">
-  <div class="row">
-    <div class="col-lg-6">
-      <a href="index.php" class="s-brand">
-        <img class="s-logo animated fadeInUp delay1" src="<?php echo $sysconf['template']['dir']; ?>/default/img/slims-logo.png" alt="" />
-        <h1 class="animated fadeInUp delay2"><?php echo $sysconf['library_name']; ?></h1>
-        <div class="s-brand-tagline animated fadeInUp delay3"><?php echo $sysconf['library_subname']; ?></div>
-      </a>
-    </div>
-    <div class="col-6-lg">
-      <div class="s-menu animated fadeInUp delay4">
-        <a href="#" id="show-menu" class="s-menu-toggle"><span></span></a>
-      </div>
-    </div>
-  </div>
-</header>
+<?php include "header.php"; ?>
 
 <!-- Navigation
 ============================================= -->
-<nav class="s-menu-content animated-fast">
-  <a href="#" id="hide-menu" class="s-menu-toggle"><span></span></a>
-  <h1>Menu</h1>
-  <ul>
-    <li><a href="index.php" rel="tab"><?php echo __('Home'); ?></a></li>
-    <li><a href="index.php?p=libinfo" rel="tab"><?php echo __('Library Information'); ?></a></li>
-    <li><a href="index.php?p=member" rel="tab"><?php echo __('Member Area'); ?></a></li>
-    <li><a href="index.php?p=librarian" rel="tab"><?php echo __('Librarian'); ?></a></li>
-    <li><a href="index.php?p=help" rel="tab"><?php echo __('Help on Search'); ?></a></li>
-    <li><a href="index.php?p=login" rel="tab"><?php echo __('Librarian LOGIN'); ?></a></li>
-  </ul>
-  <div class="s-menu-info">
-    <p>Thank you for using SLiMS</p>
-    <p>Library Automation With Style</p>
-  </div>
-</nav>
+<?php include "navigation.php"; ?>
 
 <!-- Content
 ============================================= -->
@@ -125,8 +94,8 @@ if (!defined('INDEX_AUTH')) {
 
         <!-- Search on Front Page
         ============================================= -->
-        <div class="s-main-search animated fadeInUp delay1">
-          <h1 class="s-main-title animated fadeInUp delay2">
+        <div class="s-main-search">
+          <h1 class="s-main-title animated fadeInUp delay1">
           <?php
               if(!isset($_GET['p'])) :
                 echo __('Collections');
@@ -149,51 +118,63 @@ if (!defined('INDEX_AUTH')) {
 
         <!-- Main
         ============================================= -->
-        <div class="s-main-content container animated fadeInUp delay2">
+        <div class="s-main-content container">
           <div class="row">
+
+            <!-- Show Result
+            ============================================= -->
             <div class="col-lg-8">
               <?php echo $main_content; ?>
             </div>
-            <div class="col-lg-4">
 
-              <!--// If Member Logged //-->
-              <?php if (utility::isMemberLogin()) { ?>
-                  <h2><?php echo __('Information'); ?></h2>
-                  <hr/>
-                <p>
-                  <?php echo $header_info; ?>
-                </p>
-              </div>
-              <?php } else { ?>
-                  <h2><?php echo __('Information'); ?></h2>
-                  <hr/>
-                  <p><?php echo $info; ?></p>
-              <?php } ?>
-              <!--// End Member Logged //-->
+            <div class="col-lg-4">
+              <?php if(isset($_GET['search'])) : ?>
+              <h2>Search Result</h2>
+              <hr>
+              <?php echo $search_result_info; ?>
+              <?php endif; ?>
+
+              <br>
+
+              <!-- If Member Logged
+              ============================================= -->
+              <?php if (utility::isMemberLogin()) : ?>
+                <h2><?php echo __('Information'); ?></h2>
+                <hr/>
+                <p><?php echo $header_info; ?></p>
+              <?php else: ?>
+                <h2><?php echo __('Information'); ?></h2>
+                <hr/>
+                <p><?php echo $info; ?></p>
+              <?php endif; ?>
+
               <br/>
 
-              <!--// Show if clustering search is enabled //-->
-              <?php if(!isset($_GET['p'])) { ?>
-              <?php if ($sysconf['enable_search_clustering']) { ?>
+              <!-- Show if clustering search is enabled
+              ============================================= -->
+              <?php
+                if(!isset($_GET['p'])) :
+                  if ($sysconf['enable_search_clustering']) : ?>
                   <h2><?php echo __('Search Cluster'); ?></h2>
                   <hr/>
-                  <div id="search-cluster"><div class="cluster-loading"><?php echo __('Generating search cluster...');  ?></div></div>
-                  <script type="text/javascript">
-                  $('document').ready( function() {
-                    $.ajax({
-                      url: 'index.php?p=clustering&q=<?php echo urlencode($criteria); ?>',
-                      type: 'GET',
-                      success: function(data, status, jqXHR) {
-                        $('#search-cluster').html(data);
-                      }
-                    });
-                  });
-                  </script>
-              </div>
-              <?php } ?>
-              <!--// End Show if clustering search is enabled //-->
-              <?php } ?>
+                  <div id="search-cluster">
+                    <div class="cluster-loading"><?php echo __('Generating search cluster...');  ?></div>
+                  </div>
 
+                  <script type="text/javascript">
+                    $('document').ready( function() {
+                      $.ajax({
+                        url     : 'index.php?p=clustering&q=<?php echo urlencode($criteria); ?>',
+                        type    : 'GET',
+                        success : function(data, status, jqXHR) {
+                                    $('#search-cluster').html(data);
+                                  }
+                      });
+                    });
+                  </script>
+
+                  <?php endif; ?>
+                <?php endif ?>
             </div>
           </div>
         </div>
@@ -201,12 +182,11 @@ if (!defined('INDEX_AUTH')) {
   </main>
 
 <?php else: ?>
-<!-- Result
-============================================= -->
-
+  <!-- Homepage
+  ============================================= -->
   <main id="content" class="s-main" role="main">
 
-        <!-- Search on Result Page
+        <!-- Search form
         ============================================= -->
         <div class="s-main-search animated fadeInUp delay1">
           <form action="index.php" method="get" autocomplete="off">
@@ -217,45 +197,26 @@ if (!defined('INDEX_AUTH')) {
           </form>
         </div>
 
-        <!-- Feature
+        <!-- Featured
         ============================================= -->
-
         <a href="#" class="s-feature animated fadeInUp delay6">see also our featured collections
           <div class="s-menu-toggle animated fadeInUp delay7"><span></span></div>
         </a>
+        <div class="s-feature-content">
+
+        </div>
+
 
   </main>
 <?php endif; ?>
 
 <!-- Footer
 ============================================= -->
-<footer class="s-footer container" role="footer">
-  <div class="row">
-    <div class="col-lg-6">
-      <div class="s-footer-tagline">
-        SLIMS 8 Akasia
-      </div>
-    </div>
-    <div class="col-lg-6">
-      <ul class="s-footer-menu">
-        <li><a target="_blank" href="http://www.facebook.com/groups/senayan.slims">Facebook</a></li>
-        <li><a target="_blank" href="http://twitter.com/#!/slims_official">Twitter</a></li>
-        <li><a target="_blank" href="http://www.youtube.com/user/senayanslims">Youtube</a></li>
-        <li><a target="_blank" href="https://github.com/slims">Github</a></li>
-        <li><a target="_blank" href="http://slims.web.id/forum">Forum</a></li>
-      </ul>
-    </div>
-  </div>
-</footer>
+<?php include "footer.php"; ?>
 
 <!-- Background
 ============================================= -->
-<div class="s-video animated fadeIn">
-  <video loop autoplay muted>
-    <source src='<?php echo $sysconf['template']['dir']; ?>/default/video/sdc.webm' type='video/webm' />
-    Your browser does not support the video tag.
-  </video>
-</div>
+<?php include "bg.php"; ?>
 
 </body>
 </html>
