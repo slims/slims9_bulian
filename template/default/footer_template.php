5e4928fd3720602f6c49bc5c8c91d2d4c6931222
Eddy Subratha <eddy.subratha@gmail.com>
2015-03-31 21:27:17 +0700

    Fixed Feature Collection layout

--- template/default/footer_template.php
+++ template/default/footer_template.php
@@ -1,4 +1,12 @@
-<footer class="s-footer container">
+<?php 
+if(isset($_GET['search']) || isset($_GET['p'])) {
+  $footer_position = 'relative';
+} else {
+  $footer_position = 'fixed';
+}
+
+?>
+<footer class="s-footer container" style="position: <?php echo $footer_position ?>;">
   <div class="row">
     <div class="col-lg-6">
       <div class="s-footer-tagline">
@@ -12,6 +20,7 @@
         <li><a target="_blank" rel="archives" href="//www.youtube.com/user/senayanslims">Youtube</a></li>
         <li><a target="_blank" rel="archives" href="//github.com/slims">Github</a></li>
         <li><a target="_blank" rel="archives" href="//slims.web.id/forum">Forum</a></li>
+        <li><a target="_blank" rel="archives" href="index.php?rss=true" title="RSS" class="rss" >RSS</a></li>
       </ul>
     </div>
   </div>
--- template/default/tinfo.inc.php
+++ template/default/tinfo.inc.php
@@ -1,6 +1,18 @@
 <?php
+// Main Setup
 $sysconf['template']['base'] 					= 'php';
 $sysconf['template']['responsive']  			= true;
 
-//run gradient animation - you may need a big memory to run it well
+// Please use at your own risk.
+// Animation need big memories. Please adjust with your computer capability 
+// ========================================================================
+
+// Run gradient animation - you may need a big memory to run it well.
 $sysconf['template']['run_gradient_animation']  = false; // true or false
+
+// Choose gradient color
+// Available color:  red, blue, green, beach, mint, purple, pink
+$sysconf['template']['default_gradient']		= 'default';
+
+// Show video or image for the background.
+$sysconf['template']['background_mode']  		= 'image'; // video, image or none
