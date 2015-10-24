99c0a7658ded62b0f9feeae85177dd7ca72d771e
Arie Nugraha <dicarve@gmail.com>
2015-10-24 21:40:15 +0700

    Update : MARC batch records export Update : Add MARC XML and MARCJSON export

--- admin/modules/bibliography/biblio_utils.inc.php
+++ admin/modules/bibliography/biblio_utils.inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (C) 2010  Arie Nugraha (dicarve@yahoo.com)
+ * Copyright (C) 2015  Arie Nugraha (dicarve@yahoo.com)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
