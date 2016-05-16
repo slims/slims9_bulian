4c2c4e77560f557f8a803ef2bcdec58dd0ca3070
ido alit <ido.alit@gmail.com>
2016-05-16 16:47:28 +0700

    fix: broken link

--- template/default/partials/footer.php
+++ template/default/partials/footer.php
@@ -49,11 +49,11 @@
       <nav class="col-lg-6 col-sm-9 col-xs-12">
         <ul class="s-footer-menu">
           <li><a href="index.php"><?php echo __('Home'); ?></a></li>
-          <li><a target="_blank" rel="archives" href="www.facebook.com/groups/senayan.slims">Facebook</a></li>
-          <li><a target="_blank" rel="archives" href="www.twitter.com/#!/slims_official">Twitter</a></li>
-          <li><a target="_blank" rel="archives" href="www.youtube.com/user/senayanslims">Youtube</a></li>
-          <li><a target="_blank" rel="archives" href="www.github.com/slims">Github</a></li>
-          <li><a target="_blank" rel="archives" href="www.slims.web.id/forum">Forum</a></li>
+          <li><a target="_blank" rel="archives" href="//www.facebook.com/groups/senayan.slims">Facebook</a></li>
+          <li><a target="_blank" rel="archives" href="//www.twitter.com/#!/slims_official">Twitter</a></li>
+          <li><a target="_blank" rel="archives" href="//www.youtube.com/user/senayanslims">Youtube</a></li>
+          <li><a target="_blank" rel="archives" href="//www.github.com/slims">Github</a></li>
+          <li><a target="_blank" rel="archives" href="//www.slims.web.id/forum">Forum</a></li>
           <li><a target="_blank" rel="archives" href="index.php?rss=true" title="RSS" class="rss" >RSS</a></li>
         </ul>
       </nav>
