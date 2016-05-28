35de6b73255dd27f4f2e94794de976da9c227cac
matlam <matz-josh@gmx.de>
2016-05-28 16:52:46 +0200

    i18n for the login form

--- lib/contents/login.inc.php
+++ lib/contents/login.inc.php
@@ -147,9 +147,9 @@ if (isset($_POST['logMeIn'])) {
     <?php } ?>
     <!-- Captcha preloaded javascript - end -->
     <form action="index.php?p=login" method="post">
-    <div class="heading1">Username</div>
+    <div class="heading1"><?php echo __('Username'); ?></div>
     <div class="login_input"><input type="text" name="userName" id="userName" class="login_input" /></div>
-    <div class="heading1">Password</div>
+    <div class="heading1"><?php echo __('Password'); ?></div>
     <div class="login_input"><input type="password" name="passWord" class="login_input" /></div>
     <!-- Captcha in form - start -->
     <?php if ($sysconf['captcha']['smc']['enable']) { ?>
