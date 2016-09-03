0266ab795934cd32db3d0ac006f4dee788e0e4fd
kouider bounama <kouiderbounama@gmail.com>
2016-09-03 12:05:12 +0100

    add clasic teaplate for rtl language
    add clasic teaplate for rtl language" arbicK,persian....."

---
+++ template/classic-dz/index_template.inc.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @Author: ido_alit
+ * @Date:   2015-11-12 18:45:37
+ * @Last Modified by:   ido_alit
+ * @Last Modified time: 2015-11-22 19:23:00
+ */
+
+/*
+=========================
+Be sure that this file not accessed directly
+=========================
+*/
+if (!defined('INDEX_AUTH')) {
+  die("can not access this file directly");
+} elseif (INDEX_AUTH != 1) {
+  die("can not access this file directly");
+} 
+
+/* 
+=========================
+Define current public template directory
+=========================
+*/
+define('CURRENT_TEMPLATE_DIR', $sysconf['template']['dir'].'/'.$sysconf['template']['theme'].'/');
+
+/* 
+=========================
+Load config template
+=========================
+*/
+include 'tinfo.inc.php';
+
+/* 
+=========================
+Load custome function
+=========================
+*/
+include 'function.php';
+
+/* 
+=========================
+Load header
+- open html tag
+- head tag
+- open body tag
+=========================
+*/
+include 'part/header.php';
+
+/* 
+=========================
+Load content
+=========================
+| You can change the layout of template part
+| by change/move/remove structure of load content part
+*/
+include 'part/content/library-name.php';
+include 'part/content/nav.php';
+
+// open row for grouping
+include 'part/content/row_open.php';
+include 'part/main-content.php';
+include 'part/sidebar.php';
+// close row
+include 'part/content/row_close.php';
+
+// include 'part/content/footer.php';
+
+/* 
+=========================
+Load footer
+- close body tag
+- close html tag
+=========================
+*/
+include 'part/footer.php';
---
+++ template/classic-dz/part/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @Author: ido_alit
+ * @Date:   2015-11-12 18:46:45
+ * @Last Modified by:   ido_alit
+ * @Last Modified time: 2015-11-24 09:35:03
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $page_title; ?></title>
+
+	<?php 
+	// load meta modified from default template
+	include 'meta.php';
+	?>
+</head>
+<body id="slims-page" class="">
+<!-- // wraper -->
+<div class="page-wraper">
