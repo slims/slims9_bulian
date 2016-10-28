ad9e4f01e7da94def3d9c2de1575d881fe294dd7
ido alit <ido.alit@gmail.com>
2016-10-28 15:11:32 +0700

    Add: select & custom template with GUI

--- template/classic/tinfo.inc.php
+++ template/classic/tinfo.inc.php
@@ -6,8 +6,32 @@
  * @Last Modified time: 2015-11-19 20:41:12
  */
 
+
 $sysconf['template']['base'] = 'php';
 $sysconf['template']['responsive'] = false;
 
+// default value
 $sysconf['template']['new-books'] = 6;
 $sysconf['template']['promoted-books'] = 10;
+
+// option
+$sysconf['template']['option'][$sysconf['template']['theme']] = array(
+  'option_1' => array(
+		'dbfield' => 'new-books',
+		'label' => 'Display number new book',
+		'type' => 'text',
+		'default' => 6,
+    'max' => '50',
+    'data' => false,
+    'width' => 50
+  ),
+  'option_2' => array(
+		'dbfield' => 'promoted-books',
+		'label' => 'Display number promoted book',
+		'type' => 'text',
+		'default' => 10,
+    'max' => '50',
+    'data' => false,
+    'width' => 50
+  )
+);
--- template/default/tinfo.inc.php
+++ template/default/tinfo.inc.php
@@ -17,3 +17,54 @@ $sysconf['template']['default_gradient']		= 'blue';
 
 // Show video or image for the background.
 $sysconf['template']['background_mode']  		= 'image'; // video, image or none
+
+
+
+// option
+$sysconf['template']['option'][$sysconf['template']['theme']] = array(
+  'option_1' => array(
+		'dbfield' => 'run_gradient_animation',
+		'label' => 'Run gradient animation',
+		'type' => 'dropdown',
+		'default' => 0, // 0 to false; 1 to true
+		'data' => array(
+			array(0, 'Disable'),
+			array(1, 'Enable')
+			)
+  ),
+  'option_2' => array(
+		'dbfield' => 'run_animation',
+		'label' => 'Run animation',
+		'type' => 'dropdown',
+		'default' => 0, // 0 to false; 1 to true
+		'data' => array(
+			array(0, 'Disable'),
+			array(1, 'Enable')
+			)
+  ),
+  'option_3' => array(
+		'dbfield' => 'default_gradient',
+		'label' => 'Gradient color',
+		'type' => 'dropdown',
+		'default' => 'blue',
+		'data' => array(
+			array('blue', 'Blue'),
+			array('red', 'Red'),
+      array('green', 'Green'),
+      array('beach', 'Beach'),
+      array('mint', 'Mint'),
+      array('purple', 'Purple'),
+      array('pink', 'Pink')
+			)
+  ),
+  'option_4' => array(
+		'dbfield' => 'background_mode',
+		'label' => 'Background mode',
+		'type' => 'dropdown',
+		'default' => 'image',
+		'data' => array(
+			array('none', 'none'),
+			array('image', 'Image'),
+      array('video', 'Video')
+  ))
+);
