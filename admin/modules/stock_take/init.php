b5f2aceb9fc2126953a6c102266d5c8279179ff5
Hendro Wicaksono <hendrowicaksono@yahoo.com>
2017-03-23 11:31:27 +0700

    Only admin can do initialization process in stocktake module.

--- admin/modules/stock_take/init.php
+++ admin/modules/stock_take/init.php
@@ -45,6 +45,9 @@ $can_write = utility::havePrivilege('stock_take', 'w');
 if (!($can_read AND $can_write)) {
     die('<div class="errorBox">'.__('You don\'t have enough privileges to access this area!').'</div>');
 }
+if ($_SESSION['uid'] != '1') {
+    die('<div class="errorBox">'.__('You must be an admin to run initialization process!').'</div>');
+}
 
 // check if there is any active stock take proccess
 $stk_q = $dbs->query('SELECT * FROM stock_take WHERE is_active=1');
