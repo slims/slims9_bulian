694ec1b0cc4c6bef62c992465c52d7b1ebaa2bb4
Hendro Wicaksono <hendrowicaksono@yahoo.com>
2017-04-27 05:21:39 +0700

    Add "Membership Type" option in: loan history, loan list by member, loan by classification.

--- admin/modules/reporting/customs/loan_history.php
+++ admin/modules/reporting/customs/loan_history.php
@@ -47,6 +47,7 @@ require SIMBIO.'simbio_GUI/paging/simbio_paging.inc.php';
 require SIMBIO.'simbio_DB/datagrid/simbio_dbgrid.inc.php';
 require MDLBS.'reporting/report_dbgrid.inc.php';
 
+$membershipTypes = membershipApi::getMembershipType($dbs);
 $page_title = 'Loan History Report';
 $reportView = false;
 $num_recs_show = 20;
@@ -74,6 +75,18 @@ if (!$reportView) {
             echo simbio_form_element::textField('text', 'id_name', '', 'style="width: 50%"');
             ?>
             </div>
+        </div>
+        <div class="divRow">
+          <div class="divRowLabel"><?php echo __('Membership Type'); ?></div>
+          <div class="divRowContent">
+            <select name="membershipType">
+              <?php 
+              foreach ($membershipTypes as $key => $membershipType) {
+                echo '<option value="'.$key.'">'.$membershipType['member_type_name'].'</option>';
+              }
+              ?>
+            </select>
+          </div>
         </div>
         <div class="divRow">
             <div class="divRowLabel"><?php echo __('Title'); ?></div>
@@ -142,6 +155,7 @@ if (!$reportView) {
     $reportgrid = new report_datagrid();
     $reportgrid->setSQLColumn('m.member_id AS \''.__('Member ID').'\'',
         'm.member_name AS \''.__('Member Name').'\'',
+        'm.member_type_id AS \''.__('Membership Type').'\'',
         'l.item_code AS \''.__('Item Code').'\'',
         'b.title AS \''.__('Title').'\'',
         'l.loan_date AS \''.__('Loan Date').'\'',
@@ -183,6 +197,14 @@ if (!$reportView) {
         $loanStatus = (integer)$_GET['loanStatus'];
         $criteria .= ' AND is_return='.$loanStatus;
     }
+
+    if ((isset($_GET['membershipType'])) AND ($_GET['membershipType'] != '0')) {
+        $membershipType = (integer)$_GET['membershipType'];
+        $criteria .= ' AND m.member_type_id='.$membershipType;
+    }
+
+
+
     if (isset($_GET['recsEachPage'])) {
         $recsEachPage = (integer)$_GET['recsEachPage'];
         $num_recs_show = ($recsEachPage >= 20 && $recsEachPage <= 200)?$recsEachPage:$num_recs_show;
@@ -192,14 +214,23 @@ if (!$reportView) {
    // callback function to show loan status
     function loanStatus($obj_db, $array_data)
     {
-        if ($array_data[6] == 0) {
+        if ($array_data[7] == 0) {
             return '<strong>'.__('On Loan').'</strong>';
         } else {
             return __('Returned');
         }
     }
+
+    function showMembershipType($obj_db, $array_data)
+    {
+      global  $membershipTypes;
+      $_member_type_id = $array_data[2];
+      return $membershipTypes[$_member_type_id]['member_type_name'];
+    }
+
     // modify column value
-    $reportgrid->modifyColumnContent(6, 'callback{loanStatus}');
+    $reportgrid->modifyColumnContent(7, 'callback{loanStatus}');
+    $reportgrid->modifyColumnContent(2, 'callback{showMembershipType}');
 
     // put the result into variables
     echo $reportgrid->createDataGrid($dbs, $table_spec, $num_recs_show);
