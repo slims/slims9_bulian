d3ab534931afab182a1d26484231d0e5acc1a508
Hendro Wicaksono <hendrowicaksono@gmail.com>
2018-12-05 22:51:07 +0700

    Remove error: undefined index sql_criteria

--- lib/biblio_list_index.inc.php
+++ lib/biblio_list_index.inc.php
@@ -80,11 +80,16 @@ class biblio_list extends biblio_list_model
     // additional SQL string
     $_add_sql_str = ' WHERE';
     // main search criteria
-    if ($this->criteria['sql_criteria']!='') {
-      $_add_sql_str .= ' ('.$this->criteria['sql_criteria'].') AND `index`.`opac_hide`=0 ';
+    if (isset($this->criteria['sql_criteria'])) {
+      if ($this->criteria['sql_criteria']!='') {
+        $_add_sql_str .= ' ('.$this->criteria['sql_criteria'].') AND `index`.`opac_hide`=0 ';
+      } else {
+        $_add_sql_str .= ' `index`.`opac_hide`=0';
+      }
     } else {
-      $_add_sql_str .= ' `index`.`opac_hide`=0';
-	  }
+      $_add_sql_str .= ' `index`.`opac_hide`=0';      
+    }
+
     // promoted flag
     // if ($this->only_promoted) { $_add_sql_str .= ' AND promoted=1'; }
 
