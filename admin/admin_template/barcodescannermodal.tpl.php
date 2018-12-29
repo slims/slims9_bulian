dc4977ed46d6cf2d78692b90ebe1fd9d0346f7ac
Eddy Subratha <eddy.subratha@gmail.com>
2018-12-29 10:17:58 +0700

    Add Barcode Scanner to Circulation & StockOpname

---
+++ admin/admin_template/barcodescannermodal.tpl.php
@@ -0,0 +1,26 @@
+<script src="<?php echo JWB ?>quaggaJS/adapter-latest.js" type="text/javascript"></script>
+<script src="<?php echo JWB ?>quaggaJS/quagga.js" type="text/javascript"></script>
+<div class="modal fade" id="barcodeModal" tabindex="-1" role="dialog" aria-labelledby="barcodeModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+    <div class="modal-content">
+    <div class="modal-header">
+        <h5 class="modal-title" id="barcodeModalLabel">Barcode Reader</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body m-0">
+        <iframe frameborder="0" height="320" id="iframeBarcodeReader"></iframe>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    </div>
+    </div>
+</div>
+</div>
+
+<script type="text/javascript">
+$('#barcodeModal').on('hidden.bs.modal', function(e){
+    $('#iframeBarcodeReader').removeAttr('src');            
+});
+</script>
