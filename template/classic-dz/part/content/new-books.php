5da7e96f7ecf2ea50881fa5f0781612c0503adbc
Waris Agung Widodo <ido.alit@gmail.com>
2020-01-17 00:29:14 +0700

    Make new-classic theme as default

--- template/classic-dz/part/content/new-books.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @Author: ido_alit
- * @Date:   2015-11-15 18:47:55
- * @Last Modified by:   ido_alit
- * @Last Modified time: 2015-11-22 19:39:32
- */
-
-echo biblioSimpleList('ORDER BY input_date DESC', $sysconf['template']['new-books'], __('New Books'));
