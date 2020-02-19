a74c79a04fecda69b4eeb887e6c9b853e2374d53
Waris Agung Widodo <ido.alit@gmail.com>
2020-02-19 21:25:03 +0700

    Fix: unset from ajax link

--- admin/default/home.php
+++ admin/default/home.php
@@ -40,7 +40,7 @@ if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
   <div class="container-fluid">
 
   <div id="alert-new-version" class="alert alert-info border-0 mt-3 hidden">
-      <strong>News!</strong> New version of SLiMS (<code id="new_version"></code>) available to <a href="https://github.com/slims/slims9_bulian/releases/latest">download</a>.
+      <strong>News!</strong> New version of SLiMS (<code id="new_version"></code>) available to <a class="notAJAX" target="_blank" href="https://github.com/slims/slims9_bulian/releases/latest">download</a>.
   </div>
 
 <?php
@@ -498,8 +498,9 @@ $(function(){
         .then(res => res.json())
         .then(res => {
             if (res.tag_name !== '<?= SENAYAN_VERSION_TAG; ?>') {
-                $('#alert-new-version').removeClass('hidden');
                 $('#new_version').text(res.tag_name);
+                $('#alert-new-version').removeClass('hidden');
+                $('#alert-new-version a').attr('href', res.html_url)
             }
         })
     <?php endif; ?>
