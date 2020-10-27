b7f6ef92c0262478de4eb3d3dd74e42b1545896f
Heru Subekti <heroe_soebekti@yahoo.co.id>
2020-10-27 22:34:22 +0700

    Add database port in installation menu

--- install/SLiMS.inc.php
+++ install/SLiMS.inc.php
@@ -181,13 +181,13 @@ class SLiMS
     return $mix_input;
   }
 
-  function createConnection($host, $user, $pass = '', $name = null)
+  function createConnection($host, $port = '3306', $user, $pass = '', $name = null)
   {
     if (is_null($this->db)) {
       if (is_null($name)) {
-        $this->db = @new mysqli($host, $user, $pass);
+        $this->db = @new mysqli($host.':'.$port, $user, $pass);
       } else {
-        $this->db = @new mysqli($host, $user, $pass, $name);
+        $this->db = @new mysqli($host.':'.$port, $user, $pass, $name);
       }
     }
     if (mysqli_connect_error()) {
