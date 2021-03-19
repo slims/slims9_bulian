779a322e6ac44fc5ce338fe8c004bf4d3dcd4d4c
idoalit <ido.alit@gmail.com>
2021-03-19 09:03:30 +0700

    Update: sql install

--- install/install.sql.php
+++ install/install.sql.php
@@ -1083,8 +1083,8 @@ CREATE TABLE `plugins` (
   `options` json NULL,
   `path` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime NOT NULL,
+  `updated_at` datetime NULL,
+  `deleted_at` datetime NULL,
   `uid` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 
