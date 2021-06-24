3e456ab7314e1ad679ee750ab4425e0d866e4de0
idoalit <ido.alit@gmail.com>
2021-06-24 13:01:16 +0700

    Validate url next destination

--- lib/contents/member.inc.php
+++ lib/contents/member.inc.php
@@ -119,7 +119,7 @@ if (isset($_POST['logMeIn']) && !$is_member_login) {
         if ($logon->valid($dbs)) {
             // write log
             utility::writeLogs($dbs, 'member', $username, 'Login', sprintf(__('Login success for member %s from address %s'),$username,$_SERVER['REMOTE_ADDR']));
-            if (isset($_GET['destination'])) {
+            if (isset($_GET['destination']) && filter_var($_GET['destination'], FILTER_VALIDATE_URL)) {
                 header("location:" . $_GET['destination']);
             } else {
                 header('Location: index.php?p=member');
