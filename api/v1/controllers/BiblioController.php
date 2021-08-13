67075336399bde890868378980848e4595bb01dd
idoalit <ido.alit@gmail.com>
2021-08-13 22:01:28 +0700

    Prevent hidden biblio to show in home page

--- api/v1/controllers/BiblioController.php
+++ api/v1/controllers/BiblioController.php
@@ -41,7 +41,7 @@ class BiblioController extends Controller
           FROM loan AS l
           LEFT JOIN item AS i ON l.item_code=i.item_code
           LEFT JOIN biblio AS b ON i.biblio_id=b.biblio_id
-          WHERE b.title IS NOT NULL
+          WHERE b.title IS NOT NULL AND b.opac_hide < 1
           GROUP BY b.biblio_id
           ORDER BY total DESC
           LIMIT {$limit}";
@@ -58,7 +58,7 @@ class BiblioController extends Controller
                 $need = $limit;
             }
 
-            $sql = "SELECT biblio_id, title, image FROM biblio ORDER BY last_update DESC LIMIT {$need}";
+            $sql = "SELECT biblio_id, title, image FROM biblio WHERE opac_hide < 1 ORDER BY last_update DESC LIMIT {$need}";
             $query = $this->db->query($sql);
             while ($data = $query->fetch_assoc()) {
                 $data['image'] = $this->getImagePath($data['image']);
@@ -74,7 +74,7 @@ class BiblioController extends Controller
         $limit = 6;
 
         $sql = "SELECT biblio_id, title, image
-          FROM biblio
+          FROM biblio WHERE opac_hide < 1
           ORDER BY last_update DESC
           LIMIT {$limit}";
 
@@ -90,7 +90,7 @@ class BiblioController extends Controller
 
     public function getTotalAll()
     {
-        $query = $this->db->query("SELECT COUNT(biblio_id) FROM biblio");
+        $query = $this->db->query("SELECT COUNT(biblio_id) FROM biblio WHERE opac_hide < 1");
         parent::withJson([
             'data' => ($query->fetch_row())[0]
         ]);
@@ -100,7 +100,7 @@ class BiblioController extends Controller
         $limit = 3;
         $sql = "SELECT b.biblio_id, b.title, b.image, b.notes
           FROM biblio AS b, mst_gmd AS g
-          WHERE b.gmd_id=g.gmd_id AND g.gmd_name='$gmd'
+          WHERE b.gmd_id=g.gmd_id AND g.gmd_name='$gmd' AND b.opac_hide < 1
           ORDER BY b.last_update DESC
           LIMIT {$limit}";
         $query = $this->db->query($sql);
@@ -117,7 +117,7 @@ class BiblioController extends Controller
         $limit = 3;
         $sql = "SELECT b.biblio_id, b.title, b.image, b.notes
           FROM biblio AS b, item AS i, mst_coll_type AS c
-          WHERE b.biblio_id=i.biblio_id AND i.coll_type_id=c.coll_type_id AND c.coll_type_name='$coll_type'
+          WHERE b.biblio_id=i.biblio_id AND i.coll_type_id=c.coll_type_id AND c.coll_type_name='$coll_type' AND b.opac_hide < 1
           ORDER BY b.last_update DESC
           LIMIT {$limit}";
         $query = $this->db->query($sql);
