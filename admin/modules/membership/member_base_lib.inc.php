27f56fcb8e9fb0c076c997b09a7df32ff417038d
Drajat Hasan <drajathasan20@gmail.com>
2022-11-12 06:40:03 +0700

    Add member argument in overduenotice_init

--- admin/modules/membership/member_base_lib.inc.php
+++ admin/modules/membership/member_base_lib.inc.php
@@ -167,7 +167,7 @@ class member
     public function sendOverdueNotice()
     {
         // execute registered hook
-        Plugins::getInstance()->execute('overduenotice_init');
+        Plugins::getInstance()->execute('overduenotice_init', ['member' => $this]);
 
         try {
             // Template
