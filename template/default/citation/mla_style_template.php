279231da904393e93c6f284089159a3119a3b4f5
Drajat Hasan <drajathasan20@gmail.com>
2023-01-18 01:16:33 +0700

    Fix cite if author name not inverted

--- template/default/citation/mla_style_template.php
+++ template/default/citation/mla_style_template.php
@@ -2,6 +2,7 @@
 /**
  * APA Style citation
  * Copyright (C) 2015  Arie Nugraha (dicarve@gmail.com)
+ * Modification by Drajat Hasan 2023 (drajathasan20@gmail.com)
  *
  * Available data to use:
  * $author_list    : Array of authors <-- you must pre-proccess this to string first
@@ -15,13 +16,46 @@
  *
  */
 
+//  set pre-processor variable
+$author_list = [];
+$authors_string = '';
+
+// iterate some author data
+foreach ($authors as $order => $data) {
+  // chunk author name as an array based on space
+  $chunk_name = explode(' ', $data['author_name']);
+  // get last key order
+  $last_chunkname_order = array_key_last($chunk_name);
+  // set lastname
+  $last_name = $chunk_name[$last_chunkname_order];
+  // set first name
+  $first_name = $chunk_name[0]??'';
+
+  // Check everthing first name ended with comma or not
+  if (!str_ends_with(trim($first_name), ',')) {
+    unset($chunk_name[$last_chunkname_order]); // remote last chunkname
+    if ($order > 0 & count($authors) > 2) continue; // don't make it pain, just say it et al if author > 2
+    // Process for inverting name
+    $author_list[] = $last_name . ', ' . implode(', ', array_map(fn($name) => ucfirst($name) . '', $chunk_name)) . '.';
+  } else {
+    // Same as above
+    if ($order > 0 & count($authors) > 2) continue;
+    unset($chunk_name[0]);
+    // if author have comma/before it already inverted
+    $author_list[] = $first_name . ' ' . implode(', ', array_map(fn($name) => ucfirst($name) . '', $chunk_name)) . '.';
+  }
+}
+
+// glue all author data into one string
+$authors_string = implode(', ', $author_list) . (count($authors) > 2 ? ' et al.' : '');
+
 ?>
-<p class="citation">
+<p class="citation text-justify">
   <h3><?php echo __('MLA Style'); ?></h3>
   <?php if ($authors_string) : ?>
-    <span class="authors"><?php print $authors_string ?>.</span>
-    <span class="title"><em><?php print $title ?></em>.</span>
-    <span class="edition"><?php print $edition ?></span>
+    <span class="authors"><?php print $authors_string ?></span>
+    <span class="title">"<?php print $title ?>".</span>
+    <span class="edition"><em><?php print $edition ?></em></span>
   <?php else : ?>
     <span class="title"><em><?php print $title ?></em>.</span>
     <span class="edition"><?php print $edition ?>.</span>
