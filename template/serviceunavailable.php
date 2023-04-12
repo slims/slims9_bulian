8fd8afc8d50980390c0abbc3b134d83f240ac533
Drajat Hasan <drajathasan20@gmail.com>
2023-04-12 10:41:43 +0700

    Add service unavailable template

---
+++ template/serviceunavailable.php
@@ -0,0 +1,15 @@
+<!DOCTYPE Html>
+<html>
+    <head>
+        <title>Service Unvailable</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="alert alert-danger text-left d-flex align-items-center">
+            <div>
+                <h4 class="alert-heading">The Application Environment is not set correctly.</h4>
+                <p>Call system administrator to fix it.</p>
+            </div>
+        </div>
+    </body>
+</html>
