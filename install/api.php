9a7f60432d7a799efcb953f13842f9b9361236bd
Drajat Hasan <drajathasan20@gmail.com>
2023-04-14 13:18:04 +0700

    Change PHP version to 8.2

--- install/api.php
+++ install/api.php
@@ -56,7 +56,7 @@ if (isset($_GET['storeage_engines'])) {
 
 switch ($action) {
   case 'system-requirement':
-    $php_minimum_version = '8.1';
+    $php_minimum_version = '8.2';
     $check_dir = $slims->chkDir();
     $data = [
       'is_pass' => $slims->isPhpOk($php_minimum_version) &&
