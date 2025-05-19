65948307c9de6a26f71e6034db17cf44173c610e
Waris Agung Widodo <ido.alit@gmail.com>
2025-05-19 14:30:40 +0700

    Add FormAjax and HasCustomField classes with custom field handling functionality

---
+++ lib/Form/FormAjax.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * File: FormAjax.php                                                          *
+ * Project: Form                                                               *
+ * Created Date: Monday, May 19th 2025, 10:35:23 am                            *
+ * Author: Waris Agung Widodo <ido.alit@gmail.com>                             *
+ * -----                                                                       *
+ * Last Modified: Mon May 19 2025                                              *
+ * Modified By: Waris Agung Widodo                                             *
+ * -----                                                                       *
+ * Copyright (c) 2025 Waris Agung Widodo                                       *
+ * -----                                                                       *
+ * HISTORY:                                                                    *
+ * Date      	By	Comments                                                   *
+ * ----------	---	---------------------------------------------------------  *
+ */
+
+namespace SLiMS\Form;
+
+use simbio_form_table_AJAX;
+
+class FormAjax extends simbio_form_table_AJAX {}
