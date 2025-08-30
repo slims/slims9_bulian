938956d9fad40728f982e702b78552189ae9db08
Hendro Wicaksono <hendro.wicaksono@gmail.com>
2025-08-30 08:00:19 +0000

    Fixed: add submenu access to printed_card.php

--- admin/modules/bibliography/printed_card.php
+++ admin/modules/bibliography/printed_card.php
@@ -58,6 +58,12 @@ $can_read = utility::havePrivilege('bibliography', 'r');
 if (!$can_read) {
     die('<div class="errorBox">'.__('You are not authorized to view this section').'</div>');
 }
+# CHECK ACCESS
+if ($_SESSION['uid'] != 1) {
+    if (!utility::haveAccess('bibliography.catalog-printing')) {
+        die('<div class="errorBox">' . __('You are not authorized to view this section') . '</div>');
+    }
+}
 
 $max_print = 50;
 
