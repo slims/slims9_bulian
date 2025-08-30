ffdbadf49a41940ede1a6dfac20d03d57f1233a3
Hendro Wicaksono <hendro.wicaksono@gmail.com>
2025-08-30 07:41:12 +0000

    Fixed: add submenu access to dl_print.php

--- admin/modules/bibliography/dl_print.php
+++ admin/modules/bibliography/dl_print.php
@@ -42,6 +42,12 @@ $can_read = utility::havePrivilege('bibliography', 'r');
 if (!$can_read) {
     die('<div class="errorBox">'.__('You are not authorized to view this section').'</div>');
 }
+# CHECK ACCESS
+if ($_SESSION['uid'] != 1) {
+    if (!utility::haveAccess('bibliography.labels-printing')) {
+        die('<div class="errorBox">' . __('You are not authorized to view this section') . '</div>');
+    }
+}
 
 $max_print = 50;
 $print_count_item = 0;
