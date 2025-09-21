e622eaaf599cd1e0f595b98de8eb99e8e0d4dc08
Waris Agung Widodo <ido.alit@gmail.com>
2025-09-21 17:12:20 +0700

    refactor: Remove loan history triggers from SQL installation scripts

--- install/senayan_ddl.sql
+++ install/senayan_ddl.sql
@@ -425,48 +425,6 @@ CREATE TABLE `loan` (
   `uid` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Triggers `loan`
---
-DELIMITER $$
-CREATE TRIGGER `delete_loan_history` AFTER DELETE ON `loan` FOR EACH ROW DELETE FROM `loan_history` WHERE loan_id=OLD.loan_id
-$$
-DELIMITER ;
-DELIMITER $$
-CREATE TRIGGER `insert_loan_history` AFTER INSERT ON `loan` FOR EACH ROW INSERT INTO loan_history
-     SET loan_id=NEW.loan_id,
-     item_code=NEW.item_code,
-     member_id=NEW.member_id,
-     loan_date=NEW.loan_date,
-     due_date=NEW.due_date,
-     renewed=NEW.renewed,
-     is_lent=NEW.is_lent,
-     is_return=NEW.is_return,
-     return_date=NEW.return_date,
-     input_date=NEW.input_date,
-     last_update=NEW.last_update,
-     title=(SELECT b.title FROM biblio b LEFT JOIN item i ON i.biblio_id=b.biblio_id WHERE i.item_code=NEW.item_code),
-     biblio_id=(SELECT b.biblio_id FROM biblio b LEFT JOIN item i ON i.biblio_id=b.biblio_id WHERE i.item_code=NEW.item_code),
-     call_number=(SELECT IF(i.call_number IS NULL, b.call_number,i.call_number) FROM biblio b LEFT JOIN item i ON i.biblio_id=b.biblio_id WHERE i.item_code=NEW.item_code),
-     classification=(SELECT b.classification FROM biblio b LEFT JOIN item i ON i.biblio_id=b.biblio_id WHERE i.item_code=NEW.item_code),
-     gmd_name=(SELECT g.gmd_name FROM biblio b LEFT JOIN item i ON i.biblio_id=b.biblio_id LEFT JOIN mst_gmd g ON g.gmd_id=b.gmd_id WHERE i.item_code=NEW.item_code),
-     language_name=(SELECT l.language_name FROM biblio b LEFT JOIN item i ON i.biblio_id=b.biblio_id LEFT JOIN mst_language l ON b.language_id=l.language_id WHERE i.item_code=NEW.item_code),
-     location_name=(SELECT ml.location_name FROM item i LEFT JOIN mst_location ml ON i.location_id=ml.location_id WHERE i.item_code=NEW.item_code),
-     collection_type_name=(SELECT mct.coll_type_name FROM mst_coll_type mct LEFT JOIN item i ON i.coll_type_id=mct.coll_type_id WHERE i.item_code=NEW.item_code),
-     member_name=(SELECT m.member_name FROM member m WHERE m.member_id=NEW.member_id),
-     member_type_name=(SELECT mmt.member_type_name FROM mst_member_type mmt LEFT JOIN member m ON m.member_type_id=mmt.member_type_id WHERE m.member_id=NEW.member_id)
-$$
-DELIMITER ;
-DELIMITER $$
-CREATE TRIGGER `update_loan_history` AFTER UPDATE ON `loan` FOR EACH ROW UPDATE loan_history 
-    SET is_lent=NEW.is_lent,
-    is_return=NEW.is_return,
-    renewed=NEW.renewed,
-    return_date=NEW.return_date
-    WHERE loan_id=NEW.loan_id
-$$
-DELIMITER ;
-
 -- --------------------------------------------------------
 
 --
