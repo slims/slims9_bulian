71d626e9c0c7f6d98810f1ea5c2c75a86f7b526b
Heru subekti <heroe_soebekti@yahoo.co.id>
2025-10-17 21:06:17 +0700

    Improve Item Code Pattern UI and CRUD Functionality

--- admin/modules/master_file/item_code_pattern.php
+++ admin/modules/master_file/item_code_pattern.php
@@ -59,9 +59,12 @@ if (isset($_POST['itemID']) AND !empty($_POST['itemID']) AND isset($_POST['itemA
     $patterns = @unserialize($pattern_d[0]);
     foreach ($_POST['itemID'] as $pattern_id) {
         $key = array_search(trim($pattern_id), $patterns);
-        unset($patterns[$key]);
-        $data_serialize = serialize($patterns);
+        if ($key !== false) {
+             unset($patterns[$key]);
+        }
     }
+    // Re-index dan serialize array
+    $data_serialize = serialize(array_values($patterns));
 
     // update
     $update = $dbs->query('UPDATE setting SET setting_value=\''.$data_serialize.'\' WHERE setting_name=\'batch_item_code_pattern\'');
@@ -108,25 +111,29 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
 
         $table->table_attr = 'align="center" class="s-table table" cellpadding="5" cellspacing="0"';
 
-        if(count($patterns) > 0){
+        if(is_array($patterns) && count($patterns) > 0){
 
             echo  '<div class="p-3">
             <input value="'.__('Delete Selected Data').'" class="button btn btn-danger btn-delete" type="button"> 
             <input value="'.__('Check All').'" class="check-all button btn btn-default" type="button"> 
             <input value="'.__('Uncheck All').'" class="uncheck-all button btn btn-default" type="button"></div>';
-            // table header
-            $table->setHeader(array(__('Select'),__('Item Code Pattern')));
+
+            $table->setHeader(array(__('DELETE'), __('EDIT'),__('Item Code Pattern')));
             $table->table_header_attr = 'class="alterCell font-weight-bold"';
             $table->setCellAttr(0, 0, '');
             // initial row count
             $row = 1;
             foreach ($patterns  as $pattern) {
                 $cb = '<input type="checkbox" name="pattern" value="'.$pattern.'">';
-                $link = '<a href="'.MWB.'bibliography/pop_pattern.php?in=master" class="notAJAX openPopUp notIframe">'.__('Edit').'</a>';
-                $table->appendTableRow(array($cb, $pattern));
+                
+                $edit_url = MWB.'bibliography/pop_pattern.php?in=master&pattern='.urlencode($pattern);
+                $link = '<a href="'.$edit_url.'" height="500px" class="editLink notAJAX openPopUp notIframe" title="'.__('Edit pattern').'"></a>';
+
+                $table->appendTableRow(array($cb, $link, $pattern));
                 // set cell attribute
                 $table->setCellAttr($row, 0, 'class="alterCell" valign="top" style="width: 5px;"');
-                $table->setCellAttr($row, 1, 'class="alterCell2" valign="top" style="width: auto;"');
+                $table->setCellAttr($row, 1, 'class="alterCell2" valign="top" style="width: 20px;"');
+                $table->setCellAttr($row, 2, 'class="alterCell" valign="top" style="width: auto; text-align: left;"'); 
                 // add row count
                 $row++;
             }
@@ -143,14 +150,26 @@ if (isset($_POST['detail']) OR (isset($_GET['action']) AND $_GET['action'] == 'd
     $("input[type=checkbox]:checked").each(function() {
        data.push($(this).val());
     });
+    if (data.length === 0) {
+        alert("<?php echo __('Please select data to delete!'); ?>");
+        return;
+    }
+    if (!confirm("<?php echo __('Are you sure you want to delete selected pattern(s)?'); ?>")) {
+        return;
+    }
+
     $.ajax({
             url: uri,
             type: 'post',
             data: { itemID: data, itemAction: true }
         })
           .done(function (msg) {
-            parent.jQuery('#mainContent').simbioAJAX(uri)
-        })
+             alert(msg);
+             parent.jQuery('#mainContent').simbioAJAX(uri)
+          })
+          .fail(function() {
+            alert("<?php echo __('An error occurred during deletion.'); ?>");
+        });
     })
     $(".uncheck-all").on('click',function (e){
         e.preventDefault()
